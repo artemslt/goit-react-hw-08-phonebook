e48dd222f7c28a08bb6cf35b5e1390ae608a61bc
Artem <ppt.artem@gmail.com>
2022-10-13 12:53:36 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Formik, Field, Form } from 'formik';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <div>
+          <Formik
+            initialValues={{
+              name: '',
+            }}
+            onSubmit={(values, actions) => {
+              console.log('values', values);
+              this.setState(({ contacts }) =>
+                contacts.push({ id: nanoid(), name: values.name })
+              );
+              actions.setSubmitting(false);
+              actions.resetForm();
+            }}
+          >
+            <Form>
+              <label>
+                <Field type="text" name="name" />
+              </label>
+              <button type="submit">Submit</button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    );
+  }
+}
